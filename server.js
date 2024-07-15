cb691fb3fd8d0556cc3e923b64e14a81afcfcd8e
SaiyedRushan <rushan52@gmail.com>
2024-07-15 16:58:27 -0400

    feat: add logging middleware

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express"
 import routes from "./src/routes/index.js"
 import connectDB from "./src/config/database.js"
 import config from "./config.js"
+import { loggingMiddleware } from "./src/middleware/loggingMiddleware.js"
 
 const app = express()
 const port = config.PORT || 8080
@@ -9,7 +10,8 @@ const port = config.PORT || 8080
 connectDB()
 
 app.use(express.json())
-app.get("/api", routes)
+app.use(loggingMiddleware)
+app.use("/api", routes)
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
